6ef95b5b8999098050986e5249871d0df0531a27
ThePhaseless <kukubaorch@gmail.com>
2025-01-24 22:18:48 +0000

    reformat

--- ui.py
+++ ui.py
@@ -1,38 +1,77 @@
-from nicegui import ui
-import asyncio
+import datetime
 
+from nicegui import ui
+
+from models import Gender, Patient
 from patient_queue import PatientQueue
-from models import Patient, Gender
 
 queue = PatientQueue()
-position = 0
+
 ui.label("DoctorQ")
 with ui.row():
+    is_priority: ui.toggle
+    position: ui.number
+    name: ui.input
     with ui.column():
-        is_priority = ui.toggle({False: 'Zwykły', True: 'Priorytet'})
-        with ui.column().bind_visibility_from(is_priority, 'value'):
-            ui.number("Miejsce w kolejce pacjenta: ").bind_value(globals(), "position")
+        is_priority = ui.toggle(
+            {False: "Zwykły", True: "Priorytet"},
+            on_change=lambda e: ui.notify(e),
+            value=False,
+        )
+        with ui.column().bind_visibility_from(is_priority, "value"):
+            position = ui.number(
+                "Miejsce w kolejce: ",
+            )
     with ui.column():
-        name = ui.input("imię").classes('inline-flex').props('dense outlined')
-        surname = ui.input("nazwisko").classes('inline-flex').props('dense outlined')
-        pesel = ui.input("PESEL", validation = {"musi być 11 znaków": lambda e: len(e) == 11}).classes('inline-flex').props('dense outlined')
-        age = ui.number("Wiek").classes('inline-flex').props('dense outlined')
-        gender = ui.select({Gender.MALE.value: 'Mężczyzna', Gender.FEMALE.value: 'Kobieta', Gender.OTHER.value: 'Inne'}, label="Płeć", value=Gender.MALE.value).classes('inline-flex').props('dense outlined')
-        appointment_time = ui.time().classes('inline-flex').props('dense outlined')
-        async def sync():
-            await asyncio.sleep(0)
-            ui.notify("Dodano pacjenta")
-            ui.notify(gender.value)
-            patient = Patient(first_name=name.value, last_name=surname.value, pesel=pesel.value, age=age.value, gender=Gender[gender.value or ""], appointment_time=appointment_time.value)
-            ui.notify(patient.model_dump_json())
-            if is_priority:
-                queue.add_priority_patient(patient, position)
-            is_priority.set_value(any)
-            name.set_value(any)
-            surname.set_value(any)
-            pesel.set_value(any)
-            age.set_value(any)
-            gender.set_value(any)
-            appointment_time.set_value(any)
-        ui.button("Zatwierdź", on_click=sync)
+        name = ui.input("imię").classes("inline-flex").props("dense outlined")
+        surname = ui.input("nazwisko").classes("inline-flex").props("dense outlined")
+        pesel = (
+            ui.input(
+                "PESEL",
+                validation={"musi być 11 znaków": lambda e: not e or len(e) == 11},
+            )
+            .classes("inline-flex")
+            .props("dense outlined")
+        )
+        age = ui.number("Wiek").classes("inline-flex").props("dense outlined")
+        gender = (
+            ui.select(
+                {
+                    Gender.MALE.value: "Mężczyzna",
+                    Gender.FEMALE.value: "Kobieta",
+                    Gender.OTHER.value: "Inne",
+                },
+                label="Płeć",
+                value=Gender.MALE.value,
+            )
+            .classes("inline-flex")
+            .props("dense outlined")
+        )
+        appointment_time = ui.time().classes("inline-flex").props("dense outlined")
+
+        def add_patient():
+            patient = Patient(
+                first_name=name.value,
+                last_name=surname.value,
+                pesel=pesel.value,
+                age=age.value,
+                gender=Gender[gender.value or ""],
+                appointment_time=datetime.datetime.today()
+                + datetime.timedelta(
+                    hours=int(appointment_time.value.split(":")[0] or 0),
+                    minutes=int(appointment_time.value.split(":")[1] or 0),
+                ),
+            )
+            ui.notify(f"Dodano pacjenta: {patient.first_name} {patient.last_name}")
+            if is_priority and position:
+                queue.add_priority_patient(patient, position.value)
+                is_priority.set_value(None)
+            name.set_value(None)
+            surname.set_value(None)
+            pesel.set_value(None)
+            age.set_value(None)
+            gender.set_value(None)
+            appointment_time.set_value(None)
+
+        ui.button("Zatwierdź", on_click=add_patient)
 ui.run()
