58c9417de09754fcc92874a229ce3147740a6f83
ThePhaseless <kukubaorch@gmail.com>
2025-01-24 20:32:54 +0000

    use datetime and fields

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 import datetime
 import enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Gender(str, enum.Enum):
     MALE = "male"
     FEMALE = "female"
+    OTHER = "other"
 
 class Patient(BaseModel):
-    name: str
-    surname: str
-    age: int
-    pesel: str
+    first_name: str
+    last_name: str
+    pesel: str = Field(..., pattern="^\\d{11}$")
+    age: int = Field(..., gt=0)
     gender: Gender
-    is_priority: bool
-    appointment_time: datetime.time
+    appointment_time: datetime.datetime = Field(default_factory=datetime.datetime.now)
