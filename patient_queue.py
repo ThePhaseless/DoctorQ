2311c0bbabd736e0809f4e93eb6b8782fe33ba67
ThePhaseless <kukubaorch@gmail.com>
2025-01-26 23:21:59 +0000

    added time shift

--- patient_queue.py
+++ patient_queue.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Optional
 
 from models import Patient
@@ -17,34 +18,54 @@ class PatientQueue:
         """Add a patient to the end of the queue."""
         new_node = Node(patient)
         if not self.head:
+            # Round to the nearest 30 minutes
+            new_node.patient.appointment_time += datetime.timedelta(
+                minutes=60 - new_node.patient.appointment_time.minute
+            )
             self.head = new_node
         else:
             current = self.head
             while current.next:
                 current = current.next
+            new_node.patient.appointment_time = (
+                current.patient.appointment_time + datetime.timedelta(minutes=30)
+            )
             current.next = new_node
 
     def add_priority_patient(self, patient: Patient, position: int):
         """Add a priority patient at a specific position in the queue."""
-        new_node = Node(patient)
-        if position <= 0 or not self.head:
-            # Insert at the head if position is <= 0 or the list is empty
-            new_node.next = self.head
-            self.head = new_node
+        if not self.head:
+            self.add_patient(patient)
             return
 
-        current = self.head
-        prev = None
-        current_position = 0
+        new_node = Node(patient)
+        if position <= 0:
+            # Insert at the head if position is <= 0 or the list is empty
+            new_node.patient.appointment_time = self.head.patient.appointment_time
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            prev = None
+            current_position = 0
 
-        while current and current_position < position - 1:
-            prev = current
+            while current and current_position < position:
+                prev = current
+                current = current.next
+                current_position += 1
+
+            if not current:
+                raise
+
+            new_node.patient.appointment_time = current.patient.appointment_time
+            new_node.next = current
+            if prev:
+                prev.next = new_node
+
+        current = new_node
+        while current and current.next:
             current = current.next
-            current_position += 1
-
-        new_node.next = current
-        if prev:
-            prev.next = new_node
+            current.patient.appointment_time += datetime.timedelta(minutes=30)
 
     def remove_patient(self, pesel: str):
         """Remove a patient from the queue by their PESEL."""
